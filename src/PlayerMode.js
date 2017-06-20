9babcc4a9df67953120058994fdc5f387a44421c
Michael Luo <michaelluo98@gmail.com>
2017-06-20 11:15:28 -0700

    Polish PlayerMode styling

--- src/PlayerMode.js
+++ src/PlayerMode.js
@@ -4,12 +4,18 @@ import MenuItem from 'material-ui/MenuItem';
 
 const styles = {
   customWidth: {
-	    width: 140,
-		/*height: 100*/
-		marginLeft: 0, 
-		marginRight: 0
+	    width: 110,
+		height: 40
   },
 };
+
+const lineStyle = {
+  height: 8
+}
+
+const wordStyle = {
+  height: 45
+}
 
 export default class PlayerMode extends React.Component {
 
@@ -29,6 +35,8 @@ export default class PlayerMode extends React.Component {
 	            onChange={this.handleChange}
 	            style={styles.customWidth}
 	            autoWidth={false}
+				/*underlineStyle={lineStyle}*/
+				/*labelStyle={wordStyle}*/
 	          >
 	            <MenuItem value={1} primaryText="all" />
 	            <MenuItem value={2} primaryText="5 v 5" />
