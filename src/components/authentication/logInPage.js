201319bcaec9059e391182aafe28bed7b6619974
Michael Luo <michaelluo98@gmail.com>
2017-07-12 10:28:33 -0700

    Start styling login page

--- src/components/authentication/logInPage.js
+++ src/components/authentication/logInPage.js
@@ -5,10 +5,11 @@ import Paper from 'material-ui/Paper';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import * as sessionActions from '../../actions/sessionActions';
+import BallLogo from '../../icons/basketball.svg';
 
 const styles = {
 	paperStyle: {
-		height: '400px', 
+		height: '220px', 
 		width: '300px', 
 		display: 'flex', 
 		alignItems: 'center', 
@@ -18,6 +19,24 @@ const styles = {
 		display: 'flex',
 		alignItems: 'center', 
 		justifyContent: 'center', 
+	}, 
+	logoStyle: { 
+		display: 'inline-block', 
+	}, 
+	newAccountStyle: {
+		height: '50px', 
+		width: '300px', 
+		marginTop: '20px', 
+		display: 'flex', 
+		justifyContent: 'center', 
+		alignItems: 'center'
+	},
+	introStyle: {
+		height: '100px', 
+		width: '300px', 
+		display: 'flex', 
+		alignItems: 'center',
+		justifyContent: 'center',
 	}
 }
 
@@ -39,39 +58,49 @@ class LogInPage extends Component {
   onSave(event) {
     event.preventDefault();
     this.props.actions.logInUser(this.state.credentials);
-		//console.log('------------------context:', this.context);
-		//console.log('------------------context:', this.context.router);
-		//console.log('-----------------props:', this.props.history)
 		this.props.history.push('/');
-		//this.context.router.push('/');
   }
 
   render() {
     return (
       <div style={styles.mainDivStyle}>
-				<Paper style={styles.paperStyle} zDepth={3} rounded={false}>
-					<form>
-						<TextField
-							name="email"
-							hintText="email"
-							value={this.state.credentials.email}
-							onChange={this.onChange}
-							style={{textAlign: 'center', paddingLeft: '15px'}}/>
-						<TextField
-							name="password"
-							hintText="password"
-							type="password"
-							value={this.state.credentials.password}
-							onChange={this.onChange}
-							style={{textAlign: 'center', paddingLeft: '15px'}}/>
-						<input
-							type="submit"
-							className="btn btn-primary"
-							onClick={this.onSave}
-							style={{textAlign: 'center', marginLeft: '130px'}}/>
-					</form>
-				</Paper>
-      </div>
+				<div>
+					<div style={styles.introStyle}>
+						<svg className={`icon icon-info`} style={styles.logoStyle} width={70} height={70}>
+							<use xlinkHref={`${BallLogo}#icon-info`} />
+						</svg>
+					</div>
+					<div style={{display: 'flex', justifyContent: 'center'}}>
+						<h3>Sign in to BallerCaller</h3>
+					</div>
+					<Paper style={styles.paperStyle} zDepth={2}>
+						<form>
+							<TextField
+								name="email"
+								hintText="email"
+								value={this.state.credentials.email}
+								onChange={this.onChange}
+								style={{textAlign: 'center', paddingLeft: '15px'}}/>
+							<TextField
+								name="password"
+								hintText="password"
+								type="password"
+								value={this.state.credentials.password}
+								onChange={this.onChange}
+								style={{textAlign: 'center', paddingLeft: '15px'}}/>
+							<input
+								type="submit"
+								className="btn btn-primary"
+								onClick={this.onSave}
+								style={{textAlign: 'center', marginLeft: '130px'}}/>
+						</form>
+					</Paper>
+					<Paper zDepth={2} style={styles.newAccountStyle}>
+						<p>New to BallerCaller? Create an account.</p>
+					</Paper>
+
+				</div>
+		  </div>
     );
   }
 }
