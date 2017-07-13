2fa9f56ee37863d0cb57cdc83b9fb9b518446ff7
Michael Luo <michaelluo98@gmail.com>
2017-07-13 13:04:45 -0700

    Finish show page styling

--- src/components/game/ShowGame.js
+++ src/components/game/ShowGame.js
@@ -2,20 +2,61 @@ import React from 'react';
 import styles from '../styles/gameStyles';
 import moment from 'moment';
 
+// fontFamily: 'Roboto, sans-serif'
+
+
+const showGameStyle = {
+	title: {
+		textAlign: 'center', 
+		marginTop: '5px', 
+		marginBottom: '2px', 
+		color: 'red'
+	}
+}
+
+function showSetting(game) {
+	const gameSetting = game.setting ? `Indoor` : `Outdoor`
+	let modeDisplay = ''
+	if (game.mode === 'threes') {
+		modeDisplay = '3 on 3'
+	}
+	else if (game.mode === 'fours') {
+		modeDisplay = '4 on 4'
+	}
+	else {
+		modeDisplay = '5 on 5'
+	}
+	return <p style={{fontSize: '12px'}}>{gameSetting} {modeDisplay}</p>
+}
+
+const showCourtInfo = (court) => {
+		return <div style={{padding: '5px'}}>
+				<h4 style={{margin: '2px', fontSize: '14px'}}>{court.name}</h4>
+				<p style={{fontSize: '12px', margin: '0px'}}>
+					{court.address}, {court.city}, {court.province} {court.postal_code}
+				</p>
+			</div>
+}
+
 function showGame({game, court, creator}) {
   return(
     <div style={styles.mainStyle}>
       <div style={styles.keyCircle} />
-      <div style={{}}>
-        <h1 style={{textAlign: 'center'}}>{game.name}</h1>
-				<div>
-					<p>GameTime:</p>
-					<h5>{moment(game.start_time).format('MMMM Do [@] h:mm:ss a')}</h5>
-					<h5>{court.name}</h5>
-					<h5>{creator.name}</h5>
+      <div style={{fontFamily: 'Roboto, sans-serif'}}>
+        <h1 style={showGameStyle.title}>{game.name}</h1>
+				<div style={{padding: '5px'}}>
+					<p style={{fontSize: '12px'}}>
+						GameTime: {moment(game.start_time).format('MMMM Do [@] h:mm:ss a')}
+					</p>
+					{showCourtInfo(court)}
+					{showSetting(game)}
+					<p style={{fontSize: '12px'}}>
+						<strong>Additional Information</strong>: {game.extra_info}
+					</p>
+					<p style={{fontSize: '12px'}}>
+						Creator: {creator.first_name} {creator.last_name}
+					</p>
 				</div>
-				{game.setting ? <h4>Indoor</h4> : <h4>Outdoor</h4>}
-				<p><strong>Additional Information</strong>: {game.extra_info}</p>
       </div>
     </div>
   )
